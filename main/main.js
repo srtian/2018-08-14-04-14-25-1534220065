a8d6fca5ae7102f6455c8803bb25407b381f66d2
Srtian <38072743+srtian@users.noreply.github.com>
2018-09-25 15:34:58 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    const array = input.toString().split('').map(Number)
+    const len = array.length
+    const led = [
+    ["._.","|.|","|_|"],
+     ["...","..|","..|"],
+     ["._.","._|","|_."], 
+     ["._.","._|","._|"],
+     ["...","|_|","..|"],
+     ["._.","|_.","._|"],
+     ["._.","|_.","|_|"],
+     ["._.","..|","..|"],
+     ["._.","|_|","|_|"],
+     ["._.","|_|","..|"],
+    ]
+    let str = ""
+    let newArray = []
+    for (let j = 0; j < 3; j++) {
+        for (let i = 0; i < len - 1; i++) {
+            str = str + led[array[i]][j]+" "
+        }
+        str = str + led[array[len-1]][j]
+        newArray.push(str, "\n")
+        str = ""
+    }
+    const result = newArray.reduce((prev, curr, idx, arr) => {
+        return prev + curr
+    })
+    console.log(result)
+    return result
 };
